b6782e9852a3d7ef8b33e0ff2512277df7d83c8c
Ande Other Account <Ubermaster123456@gmail.com>
2025-03-02 11:33:09 -0800

    precursor commit to push PokeTrove Version 2.0

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,14 +1,11 @@
 import os
 import sys
 
-# 1. Add your project’s path (the folder containing manage.py) to sys.path
 project_path = '/home/PokeTrove/PokeTrove-Official-Website'
 if project_path not in sys.path:
     sys.path.append(project_path)
 
-# 2. Tell Django where to find your settings
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 
-# 3. Let Django handle the WSGI application
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
